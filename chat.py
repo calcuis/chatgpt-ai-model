8b49b03e683d2111f63b641637a33f5959c987a9
calcuis <113646141+calcuis@users.noreply.github.com>
2024-01-05 10:27:10 +0800

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,25 @@
+from llama_cpp import Llama
+llm = Llama(model_path="chat.gguf")
+from tkinter import *
+import tkinter.scrolledtext as st
+
+root = Tk()
+root.title("chatGPT")
+root.columnconfigure([0, 1], minsize=180)
+root.rowconfigure(0, weight=2)
+root.rowconfigure(1, weight=1)
+i = Entry()
+o = st.ScrolledText()
+
+def submit(i):
+    output = llm("Q: "+str(i.get()), max_tokens=1024, echo=True)
+    answer = output['choices'][0]['text']
+    print(answer)
+    o.insert(INSERT, answer+"\n\n")
+
+btn = Button(text = "Submit", command = lambda: submit(i))
+i.grid(row=1, column=0, sticky="nsew")
+btn.grid(row=1, column=1, sticky="nsew")
+o.grid(row=0, columnspan=2, sticky="nsew")
+
+root.mainloop()
